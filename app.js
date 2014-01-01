7ade3edb2c8f5544dd00a22389b105f3ec540120
Nikhil <jkdman63@gmail.com>
2014-01-01 14:06:39 -0500

    works without user auth

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var chatti = require('sports-server');
 var app = express();
 var http = require('http').createServer(app);
 
-var io = require('socket.io').listen(http);
+// var io = require('socket.io').listen(http);
 
 var args = process.argv.slice(2);
 
 var server = chatti.createServer({
-  // httpServer: http,
+  httpServer: http,
   tcpHost: args[0],
   tcpPort: args[1]
 });
@@ -42,39 +42,3 @@ mgr.on('client message', function(data) {
 app.use(express.static(path.join(__dirname, 'static')));
 
 http.listen(args[2]);
-
-// io.sockets.on('connection', function(socket) {
-//   socket.emit('message', 'hi there client');
-
-//   socket.on('message', function(data) {
-//     console.log("Data from socket.io client: " + data)
-//   });
-// });
-
-
-// 
-
-// var server = net.Server();
-// server.listen(3366, 'localhost');
-
-// server.on('connection', function(socket) {
-//   socket.write("Hi client!");
-
-//   socket.on('data', function(data) {
-//     console.log("data from client: " + data)
-//   });
-// });
-
-// setTimeout(function() {
-
-//   var connection = net.connect(3366, 'localhost', function() {
-//     // connection.write("Hello server!");
-
-//     // setInterval(function() {
-//     //   connection.write("ping")
-//     // }, 2000);
-//     // connection.on('data', function(data) {
-//     //   console.log("data from server: " + data);
-//     // })
-//   });
-// }, 2000);
